47449c48c110ddfac3a18176ee7ba65b22080736
Ese-prog <onojeta.m1602050@st.futminna.edu.ng>
2022-12-28 21:42:31 +0100

    initial commit

--- lib/Const/theme_data.dart
+++ lib/Const/theme_data.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:provider/provider.dart';
-
 class Styles{
   static ThemeData themeData(bool isDarkTheme, BuildContext context){
     return ThemeData(
